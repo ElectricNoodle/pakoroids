e222fa0752fcc06aedf83ed06377bae467a8f72e
Peter Kinnaird <peter@peterkinnaird.eu>
2016-02-26 17:38:55 +0000

    Added title frame.

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,7 +6,7 @@
   Menu.prototype = {
     create: function () {
       var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5,
-        'MENU', {font: '42px Arial', fill: '#ffffff', align: 'center'
+        'Pakoroids', {font: '42px Arial', fill: '#ffffff', align: 'center'
       });
       text.anchor.set(0.5);
       this.input.onDown.add(this.onDown, this);
