e5f30d4703910da42c3c7e5f02742c84c9c96712
steven <steven@Stream-technologies.com>
2017-02-18 17:24:54 +0000

    added highscore and started writing state changes for death

---
+++ src/js/gameover.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  function Gameover() {}
+
+  Gameover.prototype = {
+    create: function () {
+      var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5,
+        'You dead, score: ' + this.game.state.states['gameover'].score , {font: '72px Arial', fill: '#ffffff', align: 'center'
+      });
+      text.anchor.set(0.5);
+      this.input.onDown.add(this.onDown, this);
+    },
+
+    update: function () {
+      if(this.game.input.keyboard.isDown(Phaser.Keyboard.H)){
+        this.game.state.start('highscore');
+      }else if(this.game.input.keyboard.isDown(Phaser.Keyboard.R)){
+        this.game.state.start('game');
+      }
+    },
+    onDown: function () {
+      this.game.state.start('menu');
+    }
+  };
+
+  window['pakoroids'] = window['pakoroids'] || {};
+  window['pakoroids'].Gameover = Gameover;
+}());
